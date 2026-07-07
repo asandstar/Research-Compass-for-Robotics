f1c65885119e1c5425dfda2bed1294ac0d7b9118
asandstar <asandstar@users.noreply.github.com>
2026-07-07 00:00:44 +0000

    deploy: 70e790fb9da0757e4746cd8270bd8fe90172526f

--- _next/static/JssC36a_cHj0eoAwpMZta/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fareas\u002F[id]","\u002Fdetail\u002F[type]\u002F[id]","\u002Fidea\u002F[id]","\u002Fmve\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
